1994a5682aa207be0da4806fb9518c672ae5dd42
reserver7 <reserver7@gmail.com>
2024-05-14 09:56:03 +0900

    feat: 리뷰 작성 및 삭제

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -3,6 +3,7 @@ import Carousel from '@/components/hotel/Carousel'
 import Contents from '@/components/hotel/Contents'
 import useHotel from '@/components/hotel/hooks/useHotel'
 import RecommendHotels from '@/components/hotel/RecommendHotels'
+import Review from '@/components/hotel/Review'
 import Rooms from '@/components/hotel/Rooms'
 import Top from '@/components/shared/Top'
 import Map from '@components/hotel/Map'
@@ -28,6 +29,7 @@ const HotelPage = () => {
       <Contents contents={contents} />
       <Map location={location} />
       <RecommendHotels recommendHotels={recommendHotels} />
+      <Review hotelId={id} />
     </div>
   )
 }
